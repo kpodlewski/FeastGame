bf3e4c80ff02d5d9c2d8f9465a46636011b05175
kpodlewski <kpodlewski91@gmail.com>
2017-08-06 22:39:40 +0200

    music v2

--- js/out.js
+++ js/out.js
@@ -707,8 +707,8 @@ function Game(board, furry, coin, score){
             newScore.innerText = this.score;
             this.coin = new Coin();
             this.showCoin();
-            var pig = new Pig('./sounds/pig4.mp3');
-            pig.play();
+            var audio  = new Audio('./sounds/pig4.mp3');
+            audio.play();
            
         }
     };
